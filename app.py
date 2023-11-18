35c93ec19babbf020e7476eeab0f2f1b3b8b247c
Goosefisher <114020951+Goosefisher@users.noreply.github.com>
2023-11-18 12:14:38 -0500

    app.py updated

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html', message='Hello, Flask!')
+def index():
+    return render_template('templates/index.html', message='Hello, Flask!')
 
 if __name__ == 'main':
     app.run(debug=True)
