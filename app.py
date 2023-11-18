0559b8bd294d66543cb984830e3ded2d9f003de6
Goosefisher <114020951+Goosefisher@users.noreply.github.com>
2023-11-17 22:11:16 -0500

    commit pleaseee

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import pandas as pd
 
-# Test
+num_classes = 26
 
+# Creates path to training data
+train_fp, test_fp = "Project/archive/sign_mnist_train.csv", "Project/archive/sign_mnist_test.csv"
 
+train_df, test_df = pd.read_csv(train_fp), pd.read_csv(test_fp)
 
+# Creates objects for inputs (x_train) and expected outputs (y_train)
+x_train, y_train = train_df.iloc[:, :0].values, train_df['label'].values
 
+x_test, y_test = test_df.iloc[:, :0].values, test_df['label'].values
+
+# PEEEEEEE
+print("hi")
