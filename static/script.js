bde3ed992d1c7e1cd16b5715bb95fecd9b916d8d
Malak Oualid <oualid.malak05@gmail.com>
2023-11-18 13:08:03 -0500

    Please work

--- static/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('test').addEventListener('click', function() {
-        let form = document.createElement('form');
-        form.setAttribute('method', 'post');
-        form.setAttribution('action', 'submit');
-
-        document.getElementsByTagName("body").appendChild(form);
-    })
-})
