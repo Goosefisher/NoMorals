5ae0dbafaeaafb718e7c3b731db101df38514c7f
Janelle Tam <janelletam.work@gmail.com>
2023-11-18 10:59:57 -0500

    Flask test, hello world

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import pandas as pd
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras import layers
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# import pandas as pd
 
-# Test
-num_classes = 26
+from flask import Flask
 
-# Creates path to training data
-train_fp, test_fp = "Project/archive/sign_mnist_train.csv", "Project/archive/sign_mnist_test.csv"
+app = Flask(__name__)
 
-train_df, test_df = pd.read_csv(train_fp), pd.read_csv(test_fp)
+@app.route('/')
+def index():
+    return "Hello World!"
 
-# Creates objects for inputs (x_train) and expected outputs (y_train)
-x_train, y_train = train_df.iloc[:, :0].values, train_df['label'].values
+if __name__ == "__main__":
+    app.run(debug=True)
 
-x_test, y_test = test_df.iloc[:, :0].values, test_df['label'].values
+###### Training Test
+# # Test
+# num_classes = 26
 
-# PEEEEEEE
-print("hi")
+# # Creates path to training data
+# train_fp, test_fp = "Project/archive/sign_mnist_train.csv", "Project/archive/sign_mnist_test.csv"
+
+# train_df, test_df = pd.read_csv(train_fp), pd.read_csv(test_fp)
+
+# # Creates objects for inputs (x_train) and expected outputs (y_train)
+# x_train, y_train = train_df.iloc[:, :0].values, train_df['label'].values
+
+# x_test, y_test = test_df.iloc[:, :0].values, test_df['label'].values
+
+# # PEEEEEEE
+# print("hi")
